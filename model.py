89ba6e89e482ceea0b8bad79cfdc082de4f595c7
Alexandr Gavrilko <aleksandr240202@gmail.com>
2022-12-08 13:25:13 +0600

    fix: model.py
    Fixed errors in model architecture: added activation functions, batch normalization, dropout.

--- model.py
+++ model.py
@@ -13,6 +13,8 @@ class AlexNet(nn.Module):
                       kernel_size=11,
                       stride=4,
                       padding=0),
+            nn.BatchNorm2d(96),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=3,
                          stride=2)
         )
@@ -22,6 +24,8 @@ class AlexNet(nn.Module):
                       kernel_size=5,
                       stride=1,
                       padding=2),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=3,
                          stride=2)
         )
@@ -31,25 +35,35 @@ class AlexNet(nn.Module):
                       kernel_size=3,
                       stride=1,
                       padding=1),
+            nn.BatchNorm2d(384),
+            nn.ReLU(),
             nn.Conv2d(in_channels=384,
                       out_channels=384,
                       kernel_size=3,
                       stride=1,
                       padding=1),
+            nn.BatchNorm2d(384),
+            nn.ReLU(),
             nn.Conv2d(in_channels=384,
                       out_channels=256,
                       kernel_size=3,
                       stride=1,
                       padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=3,
                          stride=2)
         )
         self.classifier = nn.Sequential(
             nn.Flatten(),
+            nn.Dropout(0.5),
             nn.Linear(in_features=256 * 6 * 6,
                       out_features=4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
             nn.Linear(in_features=4096,
                       out_features=4096),
+            nn.ReLU(),
             nn.Linear(in_features=4096,
                       out_features=output_shape)
         )
